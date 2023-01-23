cc63ac6d8f1231f59011419c777f44f0dbf9ea63
SupremeRubisco <92188403+SupremeRubisco@users.noreply.github.com>
2023-01-23 15:44:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // assign the button click
+  document.querySelectorAll('.dropbtn').forEach(btn => {
+    btn.addEventListener('click', e => {
+   
+      // first close all
+      document.querySelectorAll('.dropdown-content').forEach(div => div.classList.remove('show'))
+      // open the one you want which is CLOSEST
+      e.target.closest('.dropdown').querySelector('.dropdown-content').classList.add('show');
+      
+      
+    })
+  })
+
+  // catch the click outside
+  document.addEventListener('click', e => {
+  if (e.target.classList.contains('dropbtn')) return;
+    document.querySelectorAll('.dropdown-content').forEach(div => div.classList.remove('show'))
+  })
+
+
+
+})
+document.addEventListener('DOMContentLoaded', () => {
+  // assign the button click
+  document.querySelectorAll('.drpbtn').forEach(btn => {
+    btn.addEventListener('click', e => {
+   
+      // first close all
+      document.querySelectorAll('.drpdown-content').forEach(div => div.classList.remove('show'))
+      // open the one you want which is CLOSEST
+      e.target.closest('.drpdown').querySelector('.drpdown-content').classList.add('show');
+      
+      
+    })
+  })
+
+  // catch the click outside
+  document.addEventListener('click', e => {
+  if (e.target.classList.contains('drpbtn')) return;
+    document.querySelectorAll('.drpdown-content').forEach(div => div.classList.remove('show'))
+  })
+
+
+
+})
